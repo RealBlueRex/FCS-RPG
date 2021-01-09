ebee43534471896ac1bb07f30c9d43dc3bd95482
RealBlueRex <65700898+RealBlueRex@users.noreply.github.com>
2021-01-10 01:37:39 +0900

    ""

---
+++ Assets/Scripts/ButtonMgmt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonMgmt : MonoBehaviour
+{
+    public void GotoScene(string scene)
+    {
+        try
+        {
+            SceneManager.LoadScene(scene);
+            Debug.Log("System: " + scene + "씬이 로드에 성공했습니다!");
+        } catch
+        {
+            Debug.LogWarning("System: " + scene + "씬이 로드에 실패했습니다!");
+        }
+    }
+}
