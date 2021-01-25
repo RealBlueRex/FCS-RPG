aad68cf2f557f3d58d4cff67e853520bed9c1f8b
RealBlueRex <65700898+RealBlueRex@users.noreply.github.com>
2021-01-26 02:03:50 +0900

    commit
    123

--- Assets/Scripts/AudioMgmt.cs
+++ Assets/Scripts/AudioMgmt.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class AudioMgmt : MonoBehaviour
 {
+    public AudioClip _audio;
     // Start is called before the first frame update
     void Start()
     {
