da091839ec62c05a56886bb91c7468f0b2a3edf0
RealBlueRex <65700898+RealBlueRex@users.noreply.github.com>
2021-02-09 13:47:29 +0900

    Update Healthbar

---
+++ Assets/Scripts/UIMgmt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMgmt : MonoBehaviour
+{
+    [SerializeField]
+    private Slider HPbar;
+
+    private float maxHp = 100f;
+    private float curHp = 100f;
+    float imsi;
+    void Start()
+    {
+        HPbar.value = (float)curHp / (float)maxHp;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(curHp > 0)
+            {
+                curHp -= 10;
+            } else
+            {
+                curHp = 0;
+            }
+            imsi = (float)curHp / (float)maxHp;
+            HandleHp();
+        }
+        //HandleHp();
+    }
+
+    void HandleHp()
+    {
+        HPbar.value = Mathf.Lerp(HPbar.value, imsi, Time.deltaTime * 10);  
+    }
+}
